65837779ba71bd4001b12b8509f8ee984bd72559
bala-amypo <bala@amypo.in>
2025-12-19 04:22:24 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -1,10 +1,18 @@
 package com.example.demo.entity;
 import org.springframework.stereotype.Entity;
-import jakarta.persistence.I
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.NotNull;
 @Entity
 public class UserEntity{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    private String name;
+    @Email
+    @Column(name="email",unique=true)
+    private String email;
 
 }
