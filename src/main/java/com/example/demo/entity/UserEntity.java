2a21f7a0bc2a3f02fc102278147ac94b99228a9d
bala-amypo <bala@amypo.in>
2025-12-19 04:19:24 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -1,4 +1,10 @@
 package com.example.demo.entity;
 import org.springframework.stereotype.Entity;
+import jakarta.persistence.I
 @Entity
-public class UserEntity
+public class UserEntity{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+}
