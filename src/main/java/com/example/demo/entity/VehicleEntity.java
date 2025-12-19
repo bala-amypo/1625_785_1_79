93836cb227cabdbf4f989cfdb8e53debe0f41814
bala-amypo <bala@amypo.in>
2025-12-19 04:25:20 +0000

    User commit

--- src/main/java/com/example/demo/entity/VehicleEntity.java
+++ src/main/java/com/example/demo/entity/VehicleEntity.java
@@ -1,1 +1,22 @@
+
 package com.example.demo.entity;
+import org.springframework.stereotype.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.NotNull;
+import jakarta.validation.Column;
+@Entity
+public class VehicleEntity{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String name;
+    @Email
+    @Column(name="vehicleNumber",unique=true)
+    private String vehicleNumber;
+    private Double password;
+    private String role; 
+
+}
