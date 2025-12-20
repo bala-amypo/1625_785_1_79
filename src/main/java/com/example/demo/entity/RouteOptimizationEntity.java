9acfd20cbab43ec9cb483e1206d51f16a87fc6ea
bala-amypo <bala@amypo.in>
2025-12-20 17:16:20 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/RouteOptimizationEntity.java
@@ -0,0 +1,22 @@
+
+package com.example.demo.entity;
+import org.springframework.stereotype.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.NotNull;
+import jakarta.validation.Column;
+import jakarta.validation.Min;
+import java.time.LocalDateTime;
+@Entity
+public class RouteOptimizationEntity{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private String Shipment;
+    private double optimizedDistanceKm;
+    private double estimatedFuelUsageL;
+    private LocalDateTime generatedAt;
+
+}
