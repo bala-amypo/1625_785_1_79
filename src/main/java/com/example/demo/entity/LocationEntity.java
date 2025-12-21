dadf64427180aad895b923e56a7b530c14995a0d
bala-amypo <bala@amypo.in>
2025-12-21 17:31:08 +0000

    User commit

--- src/main/java/com/example/demo/entity/LocationEntity.java
+++ src/main/java/com/example/demo/entity/LocationEntity.java
@@ -1,12 +1,12 @@
 
 package com.example.demo.entity;
-import org.springframework.stereotype.Entity;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.validation.NotNull;
-import jakarta.validation.Column;
-import jakarta.validation.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Column;
+import jakarta.validation.constraints.Max;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
