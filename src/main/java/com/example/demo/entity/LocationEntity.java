ada1cbc79f339e3ed8ffec5e629d677ca4b92612
bala-amypo <bala@amypo.in>
2025-12-21 11:17:39 +0000

    User commit

--- src/main/java/com/example/demo/entity/LocationEntity.java
+++ src/main/java/com/example/demo/entity/LocationEntity.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class VehicleEntity{
+public class LocationEntity{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/example/demo/entity/ShipmentEntity.java
+++ src/main/java/com/example/demo/entity/ShipmentEntity.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class VehicleEntity{
+public class ShipmentEntity{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
