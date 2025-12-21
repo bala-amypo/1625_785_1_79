727c97d5da74775583168229e32d41affd17af5a
bala-amypo <bala@amypo.in>
2025-12-21 20:59:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserEntity.java
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Max;
 import jakarta.persistence.ManyToOne;
+import jakarta
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Email;
 import lombok.Data;
