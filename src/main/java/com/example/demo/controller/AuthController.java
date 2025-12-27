a85317090bae0e48a39b4cade07b41a92e810e60
bala-amypo <bala@amypo.in>
2025-12-27 04:07:39 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -24,59 +24,55 @@
 //         return userService.findByEmail(email);
 //     }
 // }
-package com.example.springboot.controller;
+package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.entity.User;
+import com.example.demo.security.JwtUtil;
+import com.example.demo.service.UserService;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.springboot.Security.JwtUtil;
-import com.example.springboot.dto.AuthRequest;
-import com.example.springboot.dto.AuthResponse;
-import com.example.springboot.entity.User;
-import com.example.springboot.service.UserService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-   @Autowired
-    UserService ser;
-    @Autowired
-    PasswordEncoder encoder;
 
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    @Autowired JwtUtil util;
-
-    @PostMapping("/add")
-    public User addExtraStudent(@RequestBody ExtraStudent stu) {
-        return ser.saveExtraStudent(stu);
+    public AuthController(UserService userService,
+                          PasswordEncoder passwordEncoder,
+                          JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
+
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+
+ 
 
     @PostMapping("/login")
     public AuthResponse login(@RequestBody AuthRequest request) {
-        ExtraStudent student = ser.CheckEmail(request.getEmail());
-        if (student == null) {
-            throw new RuntimeException("User Not Found");
 
-        }
-        if (!encoder.matches(request.getPassword(), student.getPassword())) {
+        User user = userService.findByEmail(request.getEmail());
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
             throw new RuntimeException("Invalid credentials");
         }
 
-        // return "Login successful for " + student.getEmail();
-
-
-        String token = util.generateToken(
-                student.getEmail(),
-                student.getRole()
+        String token = jwtUtil.generateToken(
+                user.getEmail(),
+                user.getRole().name()
         );
 
-
-        return new AuthResponse(token, student.getRole());
-
+        return new AuthResponse(token, user.getRole().name());
     }
 }
-
