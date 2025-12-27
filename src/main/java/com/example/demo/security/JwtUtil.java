fb17f12700ad4ace2c4b0f499bf81a19ec7b1943
bala-amypo <bala@amypo.in>
2025-12-27 04:20:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.example.demo.security;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+@Component
+public class JwtUtil {
+
+    // Must be at least 256 bits for HS256
+    private static final String SECRET =
+        "sdjhgbwubwwbgwiub8QFQ8qg87G1bfewifbiuwg7iu8wefqhjk";
+
+    private final SecretKey key =
+        Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(
+                    new Date(System.currentTimeMillis() + 10 * 60 * 1000)
+                )
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
