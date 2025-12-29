610c38a10f461ae8204fa9a0ae65113ae257898b
bala-amypo <bala@amypo.in>
2025-12-29 06:26:53 +0000

    User commit

--- src/main/java/com/example/demo/controller/RouteOptimizationController.java
+++ src/main/java/com/example/demo/controller/RouteOptimizationController.java
@@ -14,7 +14,7 @@ public class RouteOptimizationController {
         this.routeService = rs;
     }
     @PostMapping("/optimize/{id}")
-    public RouteOptimizationResult optimizeResult(@PathVariable("Shipment_ID") Long id){
+    public RouteOptimizationResult optimizeResult(@PathVariable Long id){
         return routeService.optimizeRoute(id);
     }
     @GetMapping("/result/{id}")
